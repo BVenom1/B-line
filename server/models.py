bac4e748702af1a6b3b14a993d84e7f4598c39e7
BVenom1 <bvenom169@gmail.com>
2026-07-01 14:56:34 -0400

    made changes to server to include messages table, untested

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
 from datetime import datetime
 
@@ -8,24 +8,34 @@ from pydantic import BaseModel, EmailStr
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, index=True)
     email = Column(String, index=True)
     password = Column(String)
 
 class UserModel(BaseModel):
+    id: int | None = None
     name: str | None = None
     email: EmailStr | None = None
     password: str | None = None
-    def __init__(self, name, email, password):
-        super().__init__(name=name, email=email, password=password)
+    def __init__(self, id, name, email, password):
+        super().__init__(id=id, name=name, email=email, password=password)
 
 # messages
 
 class Message(Base):
-    pass
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String, index=True)
+    username = Column(String, index=True)
+    msg = Column(String, index=True)
+    timestamp = Column(DateTime, index=True)
 
 class MessageModel(BaseModel):
-    user_id: int
-    msg: str
-    timestamp: datetime
+    id: int | None = None
+    user_id: int | None = None
+    username: str | None = None
+    msg: str | None = None
+    timestamp: datetime | None = None
+    def __init__(self, id, user_id, username, msg, timestamp):
+        super().__init__(id=id, user_id=user_id, username=username, msg=msg, timestamp=timestamp)
--- server/server.py
+++ server/server.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
-from typing import Any
 from database import SessionLocal, engine
-from models import Base, User, UserModel
+from models import Base, User, UserModel, Message, MessageModel
 
 app = FastAPI()
 
@@ -27,17 +25,8 @@ def get_db():
     finally:
         db.close()
 
-# API calls
-
-class ResponseModel(BaseModel):
-    status: int = 200
-    message: str = ""
-    item: Any = None
-    def __init__(self, status, message, item):
-        super().__init__(status=status, message=message, item=item)
-
-@app.post("/signup", response_model=UserModel)
-async def signup(user: UserModel, db: Session = Depends(get_db)):
+@app.post("/signup")
+async def signup(user: UserModel, db: Session = Depends(get_db)) -> UserModel:
     db_user = User(name = user.name, email = user.email, password = user.password)
     check = db.query(User).filter(User.email == user.email).first()
     if check == None:
@@ -46,18 +35,37 @@ async def signup(user: UserModel, db: Session = Depends(get_db)):
             db.add(db_user)
             db.commit()
             db.refresh(db_user)
-            return UserModel(db_user.name, db_user.email, None)
+            return UserModel(db_user.id, db_user.name, db_user.email, None)
         else:
             raise HTTPException(400, detail="Username is taken")
     else:
         raise HTTPException(400, detail="Email is already registered. Try logging in")
 
-@app.post("/login", response_model=UserModel)
-async def login(user: UserModel, db: Session = Depends(get_db)):
+@app.post("/login")
+async def login(user: UserModel, db: Session = Depends(get_db)) -> UserModel:
     db_user = db.query(User).filter(User.email == user.email).first()
     if db_user == None:
         raise HTTPException(404, detail="User does not exist. Try signing up")
     elif user.password == db_user.password:
-        return UserModel(db_user.name, db_user.email, None)
+        return UserModel(db_user.id, db_user.name, db_user.email, None)
     else:
         raise HTTPException(401, detail="Incorrect password")
+
+@app.post("/new_message")
+async def new_message(m: MessageModel, db: Session = Depends(get_db)) -> MessageModel:
+    db_message = Message(user_id = m.user_id, username = m.username, msg = m.msg, timestamp = m.timestamp)
+    check = db.query(User).filter(User.id == m.user_id).first()
+    if check == None:
+        raise HTTPException(404, detail="Cannot post message: User does not exist")
+    elif check.name != m.username:
+        raise HTTPException(404, "Cannot post message: Username does not match")
+    else:
+        db.add(db_message)
+        db.commit()
+        db.refresh(db_message)
+        return m
+
+@app.get("latest_messages")
+async def get_latest_messages(db: Session = Depends(get_db)) -> list[MessageModel]:
+    db_latest_msgs = db.query(Message).order_by(Message.timestamp.desc()).limit(10).all()
+    return [MessageModel(m.id, m.user_id, m.username, m.msg, m.timestamp) for m in db_latest_msgs]
